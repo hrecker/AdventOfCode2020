265db245eba036b8b200c657bdb17be6fb0c952e
Henry Recker <henryjrecker@gmail.com>
2020-12-11 23:39:32 -0600

    Day 12, leaderboard position ruined by a lowercase w

---
+++ day12/part1.py
@@ -0,0 +1,48 @@
+currentDegrees = 0
+eastPos = 0
+northPos = 0
+
+def rotate(dir, degrees):
+    global currentDegrees
+    if dir == 'L':
+        degrees = 360 - degrees
+    currentDegrees += degrees
+    currentDegrees = currentDegrees % 360
+
+def move(dir, dist):
+    global eastPos, northPos
+    if dir == 'L' or dir == 'R':
+        #print("rotating " + dir + str(dist))
+        rotate(dir, dist)
+        #print("new degrees: " + str(currentDegrees))
+        return
+
+    if dir == 'F':
+        if currentDegrees == 90:
+            dir = 'S'
+        elif currentDegrees == 180:
+            dir = 'W'
+        elif currentDegrees == 270:
+            dir = 'N'
+        else:
+            dir = 'E'
+
+    if dir == 'N':
+        #print("north " + str(dist))
+        northPos += dist
+    elif dir == 'S':
+        #print("south " + str(dist))
+        northPos -= dist
+    elif dir == 'E':
+        #print("east " + str(dist))
+        eastPos += dist
+    elif dir == 'W':
+        #print("west " + str(dist))
+        eastPos -= dist
+    #print("(" + str(eastPos) + ", " + str(northPos) + ", " + str(currentDegrees) + ")")
+
+with open("input.txt") as input:
+    for line in input:
+       move(line[0], int(line[1:]))
+
+print(abs(eastPos) + abs(northPos))
---
+++ day12/part2.py
@@ -0,0 +1,46 @@
+eastPos = 0
+northPos = 0
+waypoint = [10, 1]
+
+def rotate(dir, degrees):
+    global waypoint
+    if dir == 'L':
+        degrees = 360 - degrees
+    
+    original = (waypoint[0], waypoint[1])
+    if degrees == 90:
+        waypoint = [original[1], -original[0]]
+    elif degrees == 180:
+        waypoint = [-original[0], -original[1]]
+    elif degrees == 270:
+        waypoint = [-original[1], original[0]]
+
+def move(dir, dist):
+    global eastPos, northPos, waypoint
+    if dir == 'L' or dir == 'R':
+        rotate(dir, dist)
+        return
+
+    if dir == 'F':
+        eastPos += waypoint[0] * dist
+        northPos += waypoint[1] * dist
+
+    elif dir == 'N':
+        #print("north " + str(dist))
+        waypoint[1] += dist
+    elif dir == 'S':
+        #print("south " + str(dist))
+        waypoint[1] -= dist
+    elif dir == 'E':
+        #print("east " + str(dist))
+        waypoint[0] += dist
+    elif dir == 'W':
+        #print("west " + str(dist))
+        waypoint[0] -= dist
+    #print("(" + str(eastPos) + ", " + str(northPos) + ", " + str(currentDegrees) + ")")
+
+with open("input.txt") as input:
+    for line in input:
+       move(line[0], int(line[1:]))
+
+print(abs(eastPos) + abs(northPos))
